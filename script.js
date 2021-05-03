4afb06aa6298ee56e66b4056783a4717d5bc854e
Reikua <59732710+Reikua@users.noreply.github.com>
2021-05-03 18:36:35 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,10 @@
 -------------------------------------------------- */
 var rand;
 var selected_chinese_num;
-var chinese_numbers = ['', '일', '이', '삼', '사', '오', '육', '칠', '팔', '구', '십']
-var unique_ones_places = ['', '하나', '둘', '셋', '넷', '다섯', '여섯', '일곱', '여덟', '아홉']
+var selected_unique_num;
+var chinese_numbers = ['', '일', '이', '삼', '사', '오', '육', '칠', '팔', '구', '십'];
+var unique_ones_places = ['', '하나', '둘', '셋', '넷', '다섯', '여섯', '일곱', '여덟', '아홉'];
+var unique_tens_places = ['', '열', '스물', '서른', '마흔', '쉰', '예순', '일흔', '여든', '아흔'];
 
 /* ------------------------------------------------
 * 最初に実行しておくもの
@@ -18,6 +20,7 @@ rand_num();
 function rand_num() {
   rand = Math.floor(Math.random() * 100);
 
+  /*-- --- 漢数詞に対する分岐 --- -- */
   if (rand === 0) {
     selected_chinese_num = '공';
   } else if (rand <= 10) {
@@ -30,14 +33,31 @@ function rand_num() {
     var ones_place = Number(String(rand).substr(-1));
     selected_chinese_num = chinese_numbers[tens_place] + chinese_numbers[10] + chinese_numbers[ones_place];
   }
+
+  /*-- --- 固有数詞に対する分岐 --- -- */
+  if (rand === 0) {
+    selected_unique_num = '공';
+  } else if (rand <= 9) {
+    selected_unique_num = unique_ones_places[rand];
+  } else if (rand >= 10) {
+    var unique_tens_place = Number(String(rand).substr(-2, 1));
+    var unique_ones_place = Number(String(rand).substr(-1));
+    selected_unique_num = unique_tens_places[unique_tens_place] + unique_ones_places[unique_ones_place];
+  }
 }
 
 /* ------------------------------------------------
 * 問題の音声を読み上げる関数
 -------------------------------------------------- */
 $('.listen').click(function() {
+  if ($(this).attr('name') === 'chinese') {
+    var selected_voice = selected_chinese_num;
+  } else if ($(this).attr('name') === 'unique') {
+    var selected_voice = selected_unique_num;
+  }
+
   let uttearnce = new SpeechSynthesisUtterance();
-  uttearnce.text = selected_chinese_num;
+  uttearnce.text = selected_voice;
   uttearnce.volume = 1;
   uttearnce.rate = $(this).attr('value');
   uttearnce.pitch = 1;
@@ -52,7 +72,7 @@ $('#answer').click(function() {
   $(this).hide();
   $('#next').show();
   $('#text').text(rand);
-  $('#text_hangul').text('(' + selected_chinese_num + ')');
+  $('#text_hangul').text('(' + selected_chinese_num + ',' + selected_unique_num + ')');
 });
 
 /* ------------------------------------------------
